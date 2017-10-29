58f8bb94d06f735a12a566ae3d01f15c52377fb5
Guillermo Soto <guillermo.soto@utec.edu.pe>
2017-10-29 02:32:00 -0500

    reloading...
    29-10-2017

--- engine/newGodes.go
+++ engine/newGodes.go
@@ -6,7 +6,6 @@ import (
 	"encoding/json"
 	"math"
 	"sort"
-	"time"
 )
 
 func GetNews(v []NewFormatTasks) (test []GetPlan) {
@@ -40,14 +39,13 @@ func GetNews(v []NewFormatTasks) (test []GetPlan) {
 			virtualStartTime = StoreByWeight[0].NewEarliest
 			aPosX = StoreByWeight[0].LocX
 			aPosY = StoreByWeight[0].LocY
-
 			updateX = &aPosX
 			updateY = &aPosY
 
+
 			fmt.Println("puntero inicial : ",*updateX, *updateY)
 			feasibleIDs()
-			idtaskrelease := 0
-			delete(StoreByWeight,idtaskrelease)
+			delete(StoreByWeight,0)
 			// append last values
 			StorePlan["fa"] = GetPlan{
 				0,
@@ -67,65 +65,66 @@ func GetNews(v []NewFormatTasks) (test []GetPlan) {
 				test = append(test, v)
 			}
 
-			/* Validation tasks elapsed */
-			if StoreByWeight != nil {
-				//fmt.Println("existe IDs en plan total",StoreByWeight)
-				// func evaluate nex ord task (time elapsed && score)
-				a := nextPosition(aPosX, aPosY, stepTime)
-				if StoreByWeight[a].NewTaskType == 1 {
-					nPosX = StoreByWeight[a].LocX
-					nPosY = StoreByWeight[a].LocY
-					fmt.Println("next post",a, nPosX,nPosY)
-					q := runOrdinaryTask(aPosX,aPosY,nPosX,nPosY,stepTime, a)
-					for i := 0; i < len(q); i++ {
-						//fmt.Println(q[i].LocX)
-						StorePlan["fa"] = GetPlan{
-							q[i].TimeElapsed,
-							q[i].TimeElapsed,
-							1,
-							StepPos{
+			for i := 0; i < len(idPlanTaskOrd); i++ {
+				/* Validation tasks elapsed */
+				if StoreByWeight != nil {
+					//fmt.Println("existe IDs en plan total",StoreByWeight)
+					// func evaluate nex ord task (time elapsed && score)
+					a := nextPosition(*updateX, *updateY, stepTime)
+					if StoreByWeight[a].NewTaskType != 0 {
+						nPosX = StoreByWeight[a].LocX
+						nPosY = StoreByWeight[a].LocY
+						fmt.Println("next post",a, nPosX,nPosY)
+						q := runOrdinaryTask(*updateX,*updateY,nPosX,nPosY,stepTime, a)
+						for i := 0; i < len(q); i++ {
+							//fmt.Println(q[i].LocX)
+							StorePlan["fa"] = GetPlan{
 								q[i].TimeElapsed,
-								q[i].IdTask,
-								q[i].TypeStatus,
-								q[i].LocX,
-								q[i].LocY,
-								q[i].Dist,
-								q[i].Duration,
-							},
-						}
-						for _, v := range StorePlan {
-							test = append(test, v)
-						}
+								q[i].TimeElapsed,
+								1,
+								StepPos{
+									q[i].TimeElapsed,
+									q[i].IdTask,
+									q[i].TypeStatus,
+									q[i].LocX,
+									q[i].LocY,
+									q[i].Dist,
+									q[i].Duration,
+								},
+							}
+							for _, v := range StorePlan {
+								test = append(test, v)
+							}
 
-						stepTime = q[i].TimeElapsed
-						updateX = &q[i].LocX
-						updateY = &q[i].LocY
-						fmt.Println(StorePlan)
+							stepTime = q[i].TimeElapsed
+							updateX = &q[i].LocX
+							updateY = &q[i].LocY
+							fmt.Println(StorePlan)
 
-						if q[i].LocX == StoreByWeight[a].LocX && q[i].LocY == StoreByWeight[a].LocY && q[i].Duration == StoreByWeight[a].NewDuration {
-							deleteId = a // note delete may change
+							if q[i].LocX <= StoreByWeight[a].LocX && q[i].LocY <= StoreByWeight[a].LocY && q[i].Duration == StoreByWeight[a].NewDuration {
+								deleteId = a // note delete may change
+							}
 						}
+						delete(StoreByWeight,deleteId)
+						feasibleIDs()
+						fmt.Println("delete:  ", deleteId)
+						//fmt.Println("saliendo: ",StoreByWeight[deleteId])
+						fmt.Println("puntero final : ",*updateX, *updateY)
+						fmt.Println("tarea cumplida id: ",a)
+					}else {
+						fmt.Println("noob")
 					}
-					delete(StoreByWeight,deleteId)
-					feasibleIDs()
-					//fmt.Println("restantes: ",storeIdOrdNew)
-					fmt.Println("delete:  ", deleteId)
-					fmt.Println("saliendo: ",StoreByWeight[deleteId])
-					fmt.Println("puntero final : ",*updateX, *updateY)
-					fmt.Println("tarea cumplida id: ",a)
 				}else {
-					fmt.Println("noob")
+					fmt.Println("ar u stupid?..")
 				}
-			}else {
-				fmt.Println("ar u stupid?..")
 			}
 		}else if StoreByWeight[0].NewTaskType == 0 && StoreByWeight[0].NewImportance != 0{
 			virtualStartTime = StoreByWeight[0].NewEarliest
 			fmt.Println("E task: ", virtualStartTime, "run bictch...",StoreByWeight[0])
 		}
-		time.Sleep(time.Microsecond*2000000)
-		//elapsedTime++
+		//time.Sleep(time.Microsecond*2000000)
 		elapsedTime = stepTime
+		stepTime++
 		fmt.Println(elapsedTime)
 	}
 	return
@@ -144,24 +143,24 @@ func feasibleIDs()  {
 			break
 		}
 	}
-	fmt.Println("se acabo",storeIdOrdNew)
+	//fmt.Println("se acabo",storeIdOrdNew)
 	return
 }
-func nextPosition(x1, y1, et float64) (bestPos int) {
-	//delete(StoreByWeight,1)
+func nextPosition(x1, y1 float64 , et float64) (bestPos int) {
 	// ORDINARY TASK 4 NEXT POINTS
 	for i := 0; i < len(idPlanTaskEmer); i++ {
 		delete(StoreByWeight,idPlanTaskEmer[i])
 	}
 	// SLICE Store By Weight IDs
-	//fmt.Println("evaluar si exite", StoreByWeight[2])
+	//fmt.Println("evaluar si exite", StoreByWeight[7])
 	var storeIdOrd = make([]int,len(sortByWe))
 	var bestWeight [1]int
 
 	for k, _ := range StoreByWeight {
 		storeIdOrd[k] = k
+		//fmt.Println(k)
 	}
-	for i := 0; i < len(StoreByWeight); i++ {
+	for i := 0; i < len(sortByWe); i++ {
 		if storeIdOrd[i] == 0 {
 			continue
 		}else {
@@ -170,25 +169,40 @@ func nextPosition(x1, y1, et float64) (bestPos int) {
 		}
 	}
 	fmt.Println("Lista de IDs tareas Ord por realizar",storeIdOrd)
-	fmt.Println("bestWeight", bestWeight)
+
 
 	// CRITERIA
-	xBase := StoreByWeight[0].LocX
-	yBase := StoreByWeight[0].LocY
-	xConsidered := StoreByWeight[bestWeight[0]].LocY
+	xBase := sortByWe[0].LocX
+	yBase := sortByWe[0].LocY
+	xConsidered := StoreByWeight[bestWeight[0]].LocX
 	yConsidered := StoreByWeight[bestWeight[0]].LocY
 	distBase := distance(xConsidered,yConsidered,xBase,yBase)
-	if et + distBase + StoreByWeight[bestWeight[0]].NewDuration < virtualTimeShift {
-		baseReturn = false
+	distNext := distance(x1,y1,xConsidered,yConsidered)
+	if et + distBase + distNext + StoreByWeight[bestWeight[0]].NewDuration <= virtualTimeShift - distBase {
+		fmt.Println("tiempo transcurrido:   ",et + distBase + StoreByWeight[bestWeight[0]].NewDuration)
 		bestPos = bestWeight[0]
+		fmt.Println("bestWeight: ", bestPos)
 	}else {
+		StoreByWeight[0] = NewFormatTasks{
+			sortByWe[0].NewIdTask,
+			sortByWe[0].NewIdSite,
+			sortByWe[0].NewReleasing,
+			sortByWe[0].NewEarliest,
+			sortByWe[0].NewLatest,
+			1,
+			sortByWe[0].NewImportance,
+			1,
+			sortByWe[0].LocX,
+			sortByWe[0].LocY,
+			sortByWe[0].Frequency,
+		}
 		bestPos = 0
-		baseReturn = true
 	}
+	//delete(StoreByWeight,8)
 	return
 }
 
-func runOrdinaryTask(x1, y1, x2, y2 float64, et float64, a int) (newPos []StepPos) {
+func runOrdinaryTask(x1, y1 float64, x2, y2 float64, et float64, a int) (newPos []StepPos) {
 	idTask := 1
 	typeOperationOrd := 2
 	typeTravelOrd := 1
@@ -197,16 +211,16 @@ func runOrdinaryTask(x1, y1, x2, y2 float64, et float64, a int) (newPos []StepPo
 	fmt.Println("wtf: ", x1, y1)
 	fmt.Println("tiempo de llegada: ", arrDist)
 	for i := 0.0; i < arrDist; i++ {
+		var newX2, newY2 float64
 		arrDist := distance(x1,y1,x2,y2)
 		newX1 := x1
 		newY1 := y1
-		newX2 := newX1 + (x2 - x1) / arrDist
-		newY2 := newY1 + (y2 - y1) / arrDist
+		newX2 = newX1 + (x2 - x1) / arrDist
+		newY2 = newY1 + (y2 - y1) / arrDist
 		x1 = newX2
 		y1 = newY2
 		stepDist = distance(newX1,newY1,newX2,newY2)
 		idOrd := stepDist + i + et
-		//fmt.Println(stepDist, newX2, newY2)
 		StorePos["travelOrd"] = StepPos{
 			idOrd,
 			idTask,
@@ -221,10 +235,10 @@ func runOrdinaryTask(x1, y1, x2, y2 float64, et float64, a int) (newPos []StepPo
 			newPos = append(newPos, v)
 		}
 	}
-	//fmt.Println("last task ord points: ", StorePos)
-	// Waiting Time
-	lastPostStoreX := StorePos["travelOrd"].LocX
-	lastPostStoreY := StorePos["travelOrd"].LocY
+	//lastPostStoreX := StorePos["travelOrd"].LocX
+	lastPostStoreX := x2
+	//lastPostStoreY := StorePos["travelOrd"].LocY
+	lastPostStoreY := y2
 	lastElapsedTime := StorePos["travelOrd"].TimeElapsed
 	//fmt.Println(lastPostStoreX,lastPostStoreY)
 	waitTime := StoreByWeight[a].NewDuration
@@ -258,4 +272,7 @@ func GetJsonDT(s []GetPlan) {
 	if err != nil {
 		fmt.Println("error when create JSON file")
 	}
+}
+func Round(x, unit float64) float64 {
+	return float64(int64(x/unit+0.5)) * unit
 }
